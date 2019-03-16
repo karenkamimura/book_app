0367e3ccd32280e2ff54aba46b424ae7622b8d42
karenkamimura <55820kamimura@gmail.com>
2019-03-16 18:18:13 +0900

    [finish] 4-8

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+	get 'top' => 'post#top'
+	get 'posts' => 'posts#index'
+	get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
+	patch 'posts/:id' => 'posts#update', as: 'update_post'
+	delete 'posts/:id' => 'posts#destroy', as: 'destroy_post'
+	root :to => 'posts#top'
+  resources :posts
+end
